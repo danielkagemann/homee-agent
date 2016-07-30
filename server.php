db8de5e56c6def54123c2588db3986df1c1217c7
Daniel Kagemann <info@danielkagemannn.name>
2016-07-30 22:17:13 +0200

    prepared weather and added overlay

--- server.php
+++ server.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('weatherconfig.inc');
+
 $data = @unserialize(@file_get_contents('storage/data.json'));
 if ($data === FALSE) {
     $data = array();
@@ -13,11 +15,31 @@ function T($m) {
     }
 }
 
-T('request ' . $_REQUEST['q']);
+function getWeather() {
+    $weather = @unserialize(@file_get_contents('storage/weather.json'));
+    if ($weather === FALSE) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?zip={$WEATHER_ZIP}&appid={$WEATHER_APIKEY}");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        $response = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if ($httpcode>= 200 && $httpcode < 300) {
+
+        }
+    }
+}
+
 
 if ($_REQUEST['q'] == 'info') {
     echo json_encode($data);
-    T('  ' . json_encode($data));
+} else if ($_REQUEST['q'] == 'weather') {
+    // only get new weather if last call was 10 minutes ago or storage is empty
+
+
+
 } else {
     $area = $_REQUEST['q'];
     $name = $_REQUEST['name'];
